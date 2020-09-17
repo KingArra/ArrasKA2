3906a5b4644a9c0943cc9cdb335c347271e9cae8
KingArra <67718971+KingArra@users.noreply.github.com>
2020-09-17 17:52:44 -0400

    Update bundle.js

--- js/bundle.js
+++ js/bundle.js
@@ -3933,19 +3933,19 @@
           var c = [
             [
               "Tip: You can view and edit your keybinds in the options menu.",
-              "Tip: You can play on mobile by just going to arras.io on your phone!"
+              "Tip: You can play on mobile by just going to arras-ka2.netlify.app on your phone!"
             ],
             [
               "Tip: You can have the shield and health bar be separated by going to the options menu.",
-              "Tip: If arras is having a low frame rate, you can try enabling low graphics in the options menu.",
-              "Tip: You can make traps rounded with the classic trap setting in the options menu.",
-              "Tip: You can create your own private server with the template in the link on the options menu.",
-              "Tip: You can create your own theme with the custom theme makerin the link on the options menu."
+              "Tip: If KA2 Arras is having a low frame rate, you can try enabling low graphics in the options menu.",
+              "Tip: You can make traps look like Diep with the classic trap setting in the options menu.",
+              "Tip: If you entered a Developer Token, Press H to control a Base Protector!",
+              "Join the KA2 Discord!"
             ],
             [
-              "Teaming in FFA or FFA Maze is frowned upon, but when taken to the extremes, you can be punished.",
-              "Witch hunting is when you continuously target someone and follow them. This is frowned upon, but when taken to the extremes, you can be punished.",
-              "Multiboxing is when you use a script to control multiple tanks at the same time. This is considered CHEATING and will result in a ban."
+              "Subscribe to King Arras 2!",
+              "If your a Beta Tester, pressing ` will allow you to access TESTBED!",
+              "Begging for Beta-tester will result in a ban."
             ]
           ];
           c = c[Math.floor(Math.random() * c.length)];
