56a7c8f66d489481aa40234a20669afc224127e8
KingArra <67718971+KingArra@users.noreply.github.com>
2020-10-14 10:44:08 -0400

    Update bundle.js

--- js/bundle.js
+++ js/bundle.js
@@ -4327,7 +4327,7 @@
             id: "2",
             type: "FFA Maze",
             code: "glitch-montreal-m",
-            at: p.glitch('boss-rush-ka2-arras'),
+            at: p.glitch(''),
             prefer: !0,
            // featured: 1, // For Featured Status.
 
